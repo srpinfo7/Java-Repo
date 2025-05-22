aac248d5075bd37906ed334210b868e8ebad06c1
srpinfo7 <srpinfo7@gmail.com>
2025-05-22 13:27:37 +0530

    file 2 and 3 added of basics

---
+++ src/main/java/JAVA_REPO/File2.java
@@ -0,0 +1,16 @@
+package JAVA_REPO;
+
+//Use Case: Understand variables and data types.
+
+public class File2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 3;
+		int b = 6;
+		int sum=a+b;
+		System.out.println("Sum is "+sum);
+	}
+
+}
---
+++ src/main/java/JAVA_REPO/File3.java
@@ -0,0 +1,20 @@
+package JAVA_REPO;
+
+//Check if Number is Even or Odd
+public class File3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		if(num%2==0)
+		{
+			System.out.println("Number is Even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+
+	}
+
+}
